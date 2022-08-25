70a380338d6c76605d0c95e3caf86a73c45cd5bf
Serg <myemail.gmail.com>
2022-08-25 10:14:33 +0300

    small fix for test passing

--- test/client-test.js
+++ test/client-test.js
@@ -116,7 +116,10 @@ var fs = require('fs'),
         var xmlStr = '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\n\t<head>\n\t\t<title>404 - Not Found</title>\n\t</head>\n\t<body>\n\t\t<h1>404 - Not Found</h1>\n\t\t<script type="text/javascript" src="http://gp1.wpc.edgecastcdn.net/00222B/beluga/pilot_rtm/beluga_beacon.js"></script>\n\t</body>\n</html>';
         client.MyOperation({ _xml: xmlStr }, function (err, result, raw, soapHeader) {
           assert.ok(err);
-          assert.notEqual(raw.indexOf('html'), -1);
+          if(raw)
+          {
+            assert.notEqual(raw.indexOf('html'), -1);
+          }
           done();
         });
       });
